b7f109e76e38d9bde1564abcd067019196a631b3
Tadeo Muro <tadeomuro@gmail.com>
2018-09-23 04:25:36 +0200

    simple playlist list with tracks

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-
+        <img :src="playlist.images[0].url" alt="" @click="showTracks1()">
+        <span>{{playlist.name}}</span>
+        <ul v-if="showTracks">
+            <li v-for="track in tracks" :key="track.id">{{track.track.name}}</li>
+        </ul>
     </div>
 </template>
 
@@ -9,7 +13,34 @@ import axios from 'axios'
 
 export default {
     name: 'Playlist',
-    props:{
+    props:['playlist'],
+    data(){
+        return {
+            tracks: [],
+            showTracks: false,
+            tracksAreLoaded: false,
+            token: null
+        }
     },
+    created(){
+        this.token = this.$ls.get('token')
+    },
+    methods:{
+        showTracks1(){
+            if(!this.tracksAreLoaded) this.fetchTracks()
+            this.showTracks = !this.showTracks
+        },
+        fetchTracks(href){
+            const url = href || `https://api.spotify.com/v1/playlists/${this.playlist.id}/tracks`
+            axios.get(url, {headers: {Authorization: 'Bearer ' + this.token}})
+                .then(res => {
+                    this.tracks.push(...res.data.items)
+                    if(res.data.next) this.fetchTracks(res.data.next)
+                    else this.tracksAreLoaded = true
+                })
+                .catch(err => console.log(err))
+
+        }
+    }
 }
 </script>
--- src/views/Playlists.vue
+++ src/views/Playlists.vue
@@ -1,11 +1,13 @@
 <template>
 <div>
     <h1>Playlists</h1>
+    <playlist v-for="playlist in playlists" :key="playlist.id" :playlist="playlist"></playlist>
 </div>    
 </template>
 
 <script>
 import axios from 'axios'
+import Playlist from '../components/Playlist'
 
 export default {
     name: 'Playlists',
@@ -15,15 +17,18 @@ export default {
             token: null
         }
     },
+    components: {
+        Playlist
+    },
     created(){
         console.log('hi')
         this.token = this.$ls.get('token')
-        if(this.token) console.log(this.token)//getPlaylsits()
+        if(this.token) this.getPlaylsits()
     },
     methods:{
         getPlaylsits(href){
-            let url = href || 'https://api.spotify.com/v1/me/playlists?limit=50'
-            axios.get(url)
+            const url = href || 'https://api.spotify.com/v1/me/playlists?limit=50'
+            axios.get(url,  {headers: {Authorization: 'Bearer ' + this.token}})
                 .then(res => {
                     this.playlists.push(...res.data.items)
                     console.log(`${this.playlists.length} playlists out of ${res.data.total} fetched`)
