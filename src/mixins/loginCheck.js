156051b20ac505222e4e0d3eb9253d272817da46
Tadeo Muro <tadeomuro@gmail.com>
2018-10-22 09:18:15 +0200

    added login component and testing out mixins

---
+++ src/mixins/loginCheck.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export default {
+  created() {
+      // this.hello()
+  },
+  methods: {
+    checkIfUserLoggedIn() {
+      console.log('hello from mixin!')
+    }
+  }
+}
---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Login with your Spotify account</h1>
+        <v-btn>Login</v-btn>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style scoped>
+
+div{
+    text-align: center;
+}
+
+</style>
+
