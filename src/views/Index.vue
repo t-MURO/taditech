f80660bdbc41429ca3b2108caa216937655c79f2
Tadeo <tadeomuro@gmail.com>
2018-11-11 05:45:56 +0100

    added login callback and homepage

---
+++ src/views/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>Welcome to Tadi Tech</h1>
+        <v-btn @click="redirectTo('new-releases')">New Releases</v-btn>
+        <v-btn @click="redirectTo('playlists')">Reorder Playlists</v-btn>        
+    </div>
+</template>
+
+<script>
+export default {
+    created(){
+
+    },
+    methods:{
+        redirectTo(page){
+            this.$router.push(page);
+        }
+    }
+}
+</script>
