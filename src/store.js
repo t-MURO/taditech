382b6ee47b1b10fceb23633196d2ff048d087024
Tadeo <tadeomuro@gmail.com>
2018-11-11 05:46:27 +0100

    extended vuex store

--- src/store.js
+++ src/store.js
@@ -5,12 +5,29 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    token: null
+    token: null,
+    albums: []
   },
   mutations: {
-
+    setToken(state, token){
+      state.token = token;
+    },
+    setAlbums(state, albums){
+      state.albums = albums;
+    }
   },
   actions: {
-
+    setToken(context, token){
+      context.commit('setToken', token);
+    },
+    setAlbums(context, albums){
+      albums.sort((a, b) => b.release_date > a.release_date);
+      context.commit('setAlbums', albums);
+    }
   },
+  getters: {
+    header: state => { headers: { Authorization: 'Bearer ' + state.token } },
+    albums: state => state.albums
+    // albums: state => [...state.albums].sort((a, b) => b.release_date > a.release_date)
+  }
 });
