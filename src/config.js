29550b8176737790eec4c04964f92ac7f4eb99f4
Tadeo <tadeomuro@gmail.com>
2018-11-15 18:40:23 +0100

    automatically changes auth url depending on environment

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+const DEV_BACKEND_URL = 'https://taditech-backend-dev.herokuapp.com/login';
+const PROD_BACKEND_URL = 'https://taditech-backend.herokuapp.com/login';
+
 export default {
-    AUTH_URL: 'https://taditech-backend-dev.herokuapp.com/login'
+    AUTH_URL: process.env.NODE_ENV === 'production' ? PROD_BACKEND_URL : DEV_BACKEND_URL
 }
