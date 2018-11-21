93e0fac06a8d752cc77eaa6ca2cd2840b511a26e
Tadeo <tadeomuro@gmail.com>
2018-11-22 00:33:58 +0100

    song key formating

--- src/components/Playlist.vue
+++ src/components/Playlist.vue
@@ -126,8 +126,24 @@ export default {
             return track.features ? Math.round(track.features.tempo) : '';
         },
         getKey(track){
-            return track.features ? Math.round(track.features.key) : '';
+            return track.features ? this.getKeyValue(track.features.key) : '';
         },
+        getKeyValue(key){
+            switch(key){
+                case 0: return 'C'
+                case 1: return 'C#/Db'
+                case 2: return 'D'
+                case 3: return 'D#/Eb'
+                case 4: return 'E'
+                case 5: return 'F'
+                case 6: return 'F#/Gb'
+                case 7: return 'G'
+                case 8: return 'G#/Ab'
+                case 9: return 'A'
+                case 10: return 'A#/Ab'
+                case 11: return 'B'
+            }
+        }
     },
     watch:{
         tracks: function() {
