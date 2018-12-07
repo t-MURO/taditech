c539378234268b5136349a0c03ed6f02e1fd6e6e
Tadeo Muro <tadeomuro@gmail.com>
2018-12-07 18:41:44 +0100

    added semicolons and scoped css property

--- src/views/Playlists.vue
+++ src/views/Playlists.vue
@@ -25,25 +25,25 @@ export default {
     },
     methods:{
         getPlaylists(href){
-            const url = href || 'https://api.spotify.com/v1/me/playlists?limit=50'
+            const url = href || 'https://api.spotify.com/v1/me/playlists?limit=50';
             axios.get(url, this.$store.getters.header)
                 .then(res => {
-                    this.playlists.push(...res.data.items)
-                    console.log(`${this.playlists.length} playlists out of ${res.data.total} fetched`)
-                    if(res.data.next) this.getPlaylists(res.data.next)
+                    this.playlists.push(...res.data.items);
+                    console.log(`${this.playlists.length} playlists out of ${res.data.total} fetched`);
+                    if(res.data.next) this.getPlaylists(res.data.next);
                 })
                 .catch(err => this.handleError(err, () => this.getPlaylists(url)));
         }
     },
     computed: {
         filteredPlaylists: function(){
-            return this.playlists.filter(playlist => playlist.owner.id === this.$store.getters.user.id && playlist.images.length > 0)
+            return this.playlists.filter(playlist => playlist.owner.id === this.$store.getters.user.id && playlist.images.length > 0);
         }
     }
 }
 </script>
 
-<style>
+<style scoped>
 h1{
     margin-bottom: 1em;
 }
